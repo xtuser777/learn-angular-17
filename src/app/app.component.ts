41e1b8d3356c3cc571122cffcc1d1f73e6d50df7
Lucas Oliveira <lucaoxt@gmail.com>
2023-11-28 09:23:00 -0300

    up

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,9 +6,54 @@ import { RouterOutlet } from '@angular/router';
   selector: 'app-root',
   standalone: true,
   imports: [CommonModule, RouterOutlet],
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  // template: `
+  // <div style="display: flex; justify-content: center; align-items: center; flex-direction: column; gap: 1rem;">
+  // @for(user of userDatasList; track user.id) {
+  //  @switch (user.age) {
+  //   @case (20) {
+  //     <span style="color: blue;"> {{user.name}} is {{user.profession}} and is 20 years old </span>
+  //   }
+  //   @case (30) {
+  //     <span style="color: red;"> {{user.name}} is {{user.profession}} and is 30 years old </span>
+  //   }
+  //   @case (40) {
+  //     <span style="color: brown;"> {{user.name}} is {{user.profession}} and is 40 years old </span>
+  //   }
+  //   @default {
+  //     <span> {{user.name}} is {{user.profession}} and your age is {{user.age}} </span>
+  //   }
+  //  }
+  //   } @empty {
+  //     <span style="color: red;">Dont have datas to show</span>
+  //   }
+  // </div>
+  // `,
+  template: `
+    <div
+      style="display: flex; justify-content: center; align-items: center; flex-direction: column; gap: 1rem;"
+    >
+      <button (click)="renderBlock = true">Renderizar título</button>
+      @defer(when renderBlock) {
+      <h3 style="color: red;">Element rendered on interaction</h3>
+      } @placeholder {
+      <span style="color: blue;">Conteúdo inicial do placeholder</span>
+      }
+    </div>
+  `,
+  styleUrls: [],
 })
 export class AppComponent {
-  title = 'learn-angular-17';
+  renderBlock = false;
+  title = 'learnAngular17';
+  userDatasList: Array<{
+    name: string;
+    age: number;
+    profession: string;
+    id: string;
+  }> = [
+      { age: 20, name: 'Marcos', profession: 'Software Developer', id: '123' },
+      { age: 30, name: 'Marcelo', profession: 'Software Developer', id: '456' },
+      { age: 40, name: 'Carlos', profession: 'Scrum Master', id: '789' },
+      { age: 30, name: 'Maria', profession: 'UX Designer', id: '123' },
+    ];
 }
